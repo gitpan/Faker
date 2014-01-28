5c67d4b07b4c07add0c8b9e4561e95e2427a285d
Al Newkirk <awncorp@cpan.org>
2014-01-28 11:59:08 +0000

    Import of AWNCORP/Faker-0.01 from CPAN.
    gitpan-cpan-distribution: Faker
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         AWNCORP/Faker-0.01.tar.gz
    gitpan-cpan-author:       AWNCORP
    gitpan-cpan-maturity:     released

---
+++ lib/Faker/Generator/Null.pm
@@ -0,0 +1,16 @@
+package Faker::Generator::Null;
+
+use 5.14.0;
+use feature 'unicode_strings';
+use Moo;
+use Function::Parameters;
+
+method AUTOLOAD {
+    return undef;
+}
+
+method DESTROY {
+    # noop
+}
+
+1;
