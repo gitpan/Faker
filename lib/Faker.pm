96f582af27cc4727f04f114eae3949a4b46843f9
Al Newkirk <awncorp@cpan.org>
2014-01-30 17:41:42 +0000

    Import of AWNCORP/Faker-0.03 from CPAN.
    gitpan-cpan-distribution: Faker
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         AWNCORP/Faker-0.03.tar.gz
    gitpan-cpan-author:       AWNCORP
    gitpan-cpan-maturity:     released

--- lib/Faker.pm
+++ lib/Faker.pm
@@ -7,7 +7,7 @@ use Moo;
 use Function::Parameters;
 use Faker::Factory;
 
-our $VERSION = '0.02'; # VERSION
+our $VERSION = '0.03'; # VERSION
 
 
 method maker (@arguments) {
@@ -26,7 +26,7 @@ Faker - Extensible Fake Data Generator
 
 =head1 VERSION
 
-version 0.02
+version 0.03
 
 =head1 DESCRIPTION
 
